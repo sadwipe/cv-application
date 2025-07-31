c66bd339d8bfcbadf5ead643fc7983b2679f2359
sadwipe <sadwipe@gmail.com>
2025-07-31 14:36:32 +0300

    Image fix2

--- src/data/initialFormData.js
+++ src/data/initialFormData.js
@@ -3,7 +3,7 @@ export const initialData = {
     fullName: 'Lola Lupescu',
     email: 'lola.lupescu2024@gmail.com',
     phone: '0774 492 407 (my owner)',
-    photo: '/public/caine.jpeg',
+    photo: '/caine.jpeg',
     summary:
       'Enthusiastic and highly motivated companion with a keen sense for adventure and an unbreakable loyalty to her team. Responds immediately to trigger phrases like “do you want?”, “outside”, and “dress-up”, displaying unmatched excitement and tail-wagging readiness. Skilled in walk coordination, emotional support, and spontaneous zoomies. Always dressed to impress (with or without accessories) and ready to brighten any room with her presence.',
   },
